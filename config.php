f63ebe6f49d73faaa7878b7c3dcd084ffc64a7a1
Munotasir2762 <muntasirpranto2762@gmail.com>
2024-06-06 23:57:25 +0600

    Retrive data from database

---
+++ config.php
@@ -0,0 +1,13 @@
+
+<?php
+
+$connention = new mysqli ('localhost', 'root', '', 'student_management_system');
+
+// if($connention){
+//     echo 'Connect Successfully';
+// }
+// else{
+//     die(mysqli_error($connention));
+// }
+
+?>
